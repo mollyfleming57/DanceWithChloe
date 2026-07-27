783a50226b52aad3c20f73cca85c5566dbce4fcd
Molly Fleming <mollyfleming07@gmail.com>
2026-07-27 15:17:35 -0400

    to fix the gradient for nav bar and sign up button

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburgerBtn = document.getElementById('hamburgerBtn');
   const navLinks = document.getElementById('navLinks');
-  const signupBtn = document.getElementById('signupBtn');
 
   hamburgerBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    signupBtn.classList.toggle('active');
   });
 });
